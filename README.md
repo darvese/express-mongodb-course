4da2b7336783eb21e9be9a073d0b6d4df06a162a
erwdarve <erwdarve@bouyguestelecom.fr>
2021-08-12 16:20:21 +0200

    :sparkle: Step 4

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Go Full-Stack With Node.js, Express, and MongoDB
 
 [OpenClassroom course](https://openclassrooms.com/en/courses/5614116-go-full-stack-with-node-js-express-and-mongodb)
+
+> The frontend part is given with the course
--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 require('dotenv').config();
 
 const stuffRoutes = require('./routes/stuff');
@@ -34,5 +35,6 @@ app.use(bodyParser.json());
 
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
